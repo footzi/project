648a218c1d471c3a7a925f4686b7e223321416f9
footzi <footzi@mail.ru>
2017-01-09 13:47:13 +0300

    9.01 13.47

--- src/js/Carousel.js
+++ src/js/Carousel.js
@@ -7,7 +7,7 @@ let Carousel = React.createClass ({
     render () {
         return (
          <div className="carousel-mat">
-             <a className="carousel-item" href="#one!"><img src="image/Gallery/1.png" /></a>
+             <a className="carousel-item" href="catalog.html#/angel/products/1-1"><img src="image/Gallery/1.png" /></a>
              <a className="carousel-item" href="#two!"><img src="image/Gallery/2.png" /></a>
              <a className="carousel-item" href="#three!"><img src="image/Gallery/3.png" /></a>
              <a className="carousel-item" href="#four!"><img src="image/Gallery/4.png" /></a>
